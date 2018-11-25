19d8bac61272d0ea910313c23579c4aa04dfd3fb
Unknown <ab.learn.code@gmail.com>
2018-11-25 15:47:52 +0100

    ServerConfiguration
    server configuration and methods transaction.

--- VideoGenToolSuite/src/main/DevServer.java
+++ VideoGenToolSuite/src/main/DevServer.java
@@ -1,12 +1,33 @@
 package main;
 import static spark.Spark.*;
 
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.eclipse.emf.common.util.EList;
+import org.xtext.example.mydsl.videoGen.Media;
+
+import spark.Request;
+import spark.Response;
+
 public class DevServer {
+	private VideoSeqUtils utils = new VideoSeqUtils();
+	private EList<Media> medias = new VideoGenGenerator().getMedias("example1.videogen");
+	private FfmpegEngine ffmpeg = new FfmpegEngine();
+	private Analysis analysis = new Analysis();
 
-	public static void main(String[] args) {
-		get("/", (req, res) -> {
-            return "hello from sparkjava.com";
-        });
+	private String port = "8080";
+	private String responseDataType = "application/json";
+	private String respImageType = "image/gif";
+	private String respPngType = "image/png";
+	
+	public String getVariantDuration(Request req, Response res) throws IOException, ParseException {
+		res.type(responseDataType);
+		return this.analysis.getDuration();
+
 	}
-
+	public String getPossibleVariansAndSize(Request req, Response res) {
+		res.type(responseDataType);
+		return this.analysis.getVariantsSize(medias, utils.getMediaIds(medias));
+	}
 }
