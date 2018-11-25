5ea890f40c8d3a779f0e0a7b2f1632d1a4a2eadd
Unknown <ab.learn.code@gmail.com>
2018-11-25 16:46:14 +0100

    autogenerate
    method to auto generate a compilation.

--- VideoGenToolSuite/src/main/DevServer.java
+++ VideoGenToolSuite/src/main/DevServer.java
@@ -1,8 +1,11 @@
 package main;
 import static spark.Spark.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
+import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
 import org.xtext.example.mydsl.videoGen.Media;
@@ -17,17 +20,31 @@ public class DevServer {
 	private Analysis analysis = new Analysis();
 
 	private String port = "8080";
-	private String responseDataType = "application/json";
-	private String respImageType = "image/gif";
-	private String respPngType = "image/png";
+	private String resDataType = "application/json";
+	private String resImageType = "image/gif";
+	private String resMediaType = "video/mkv";
+	private String resPngType = "image/png";
+	
+	public String getAutoGenerate(Request req, Response res) throws FileNotFoundException, UnsupportedEncodingException {
+		res.type(this.resDataType);
+		List<String> playlist = this.ffmpeg.playListFiles(medias);
+		String playlistFile = this.ffmpeg.createFfmpegPlaylist(playlist);
+		this.ffmpeg.writeToFile(playlistFile);
+		boolean doGenerate = Boolean.parseBoolean(req.queryParams("activate"));
+		System.out.println(doGenerate);
+		String filename = req.queryParams("filename");
+		System.out.println(filename);
+		this.ffmpeg.generateVideo(doGenerate, filename);
+		return this.ffmpeg.getOutputLocation();
+	}
 	
 	public String getVariantDuration(Request req, Response res) throws IOException, ParseException {
-		res.type(responseDataType);
+		res.type(this.resDataType);
 		return this.analysis.getDuration();
 
 	}
 	public String getPossibleVariansAndSize(Request req, Response res) {
-		res.type(responseDataType);
+		res.type(this.resDataType);
 		return this.analysis.getVariantsSize(medias, utils.getMediaIds(medias));
 	}
 }
