8042ffbdd022f7e94917c8f3c34a435cfa7df192
Unknown <ab.learn.code@gmail.com>
2018-11-27 19:52:48 +0100

    git generation
    added method to retrieve gif.

--- VideoGenToolSuite/src/main/DevServer.java
+++ VideoGenToolSuite/src/main/DevServer.java
@@ -1,11 +1,17 @@
 package main;
+
 import static spark.Spark.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.emf.common.util.EList;
 import org.xtext.example.mydsl.videoGen.Media;
@@ -14,37 +20,63 @@ import spark.Request;
 import spark.Response;
 
 public class DevServer {
-	private VideoSeqUtils utils = new VideoSeqUtils();
-	private EList<Media> medias = new VideoGenGenerator().getMedias("example1.videogen");
-	private FfmpegEngine ffmpeg = new FfmpegEngine();
-	private Analysis analysis = new Analysis();
 
-	private String port = "8080";
-	private String resDataType = "application/json";
-	private String resImageType = "image/gif";
-	private String resMediaType = "video/mkv";
-	private String resPngType = "image/png";
-	
-	public String getAutoGenerate(Request req, Response res) throws FileNotFoundException, UnsupportedEncodingException {
-		res.type(this.resDataType);
-		List<String> playlist = this.ffmpeg.playListFiles(medias);
-		String playlistFile = this.ffmpeg.createFfmpegPlaylist(playlist);
-		this.ffmpeg.writeToFile(playlistFile);
-		boolean doGenerate = Boolean.parseBoolean(req.queryParams("activate"));
-		System.out.println(doGenerate);
-		String filename = req.queryParams("filename");
-		System.out.println(filename);
-		this.ffmpeg.generateVideo(doGenerate, filename);
-		return this.ffmpeg.getOutputLocation();
-	}
-	
-	public String getVariantDuration(Request req, Response res) throws IOException, ParseException {
-		res.type(this.resDataType);
-		return this.analysis.getDuration();
+  private VideoSeqUtils utils = new VideoSeqUtils();
+  private EList<Media> medias = new VideoGenGenerator().getMedias("example1.videogen");
+  private FfmpegEngine ffmpeg;
+  private Analysis analysis = new Analysis();
 
-	}
-	public String getPossibleVariansAndSize(Request req, Response res) {
-		res.type(this.resDataType);
-		return this.analysis.getVariantsSize(medias, utils.getMediaIds(medias));
-	}
+  private String port = "8080";
+  private String resDataType = "application/json";
+  private String resImageType = "image/gif";
+  private String resMediaType = "video/mkv";
+  private String resPngType = "image/png";
+
+  public Integer getAutoGenerate(Request req, Response res)
+      throws IOException, InterruptedException {
+    this.ffmpeg = new FfmpegEngine();
+    List<String> playlist = this.ffmpeg.playListFiles(medias);
+    String playlistFile = this.ffmpeg.createFfmpegPlaylist(playlist);
+    this.ffmpeg.writeToFile(playlistFile);
+    boolean doGenerate = Boolean.parseBoolean(req.queryParams("activate"));
+    String filename = req.queryParams("filename");
+    this.ffmpeg.generateVideo(doGenerate, filename);
+    Path file = Paths.get(this.ffmpeg.getOutputLocation());
+    byte[] video = Files.readAllBytes(file);
+    res.status(200);
+    HttpServletResponse resp = res.raw();
+    resp.setContentType("video/mkv");
+    resp.getOutputStream().write(video);
+    resp.getOutputStream().close();
+    return 200;
+  }
+
+  public Integer getGif(Request req, Response res) throws InterruptedException, IOException {
+    res.type(this.resImageType);
+    String gifName = req.queryParams("gifname");
+    boolean generate = this.ffmpeg.generateGif(gifName);
+    if (generate) {
+      Path file = Paths.get(this.ffmpeg.getGifLocation());
+      byte[] gif = Files.readAllBytes(file);
+      HttpServletResponse resp = res.raw();
+      resp.setContentType("image/gif");
+      resp.getOutputStream().write(gif);
+      resp.getOutputStream().close();
+      return resp.getStatus();
+    } else {
+      res.status(500);
+      return res.status();
+    }
+  }
+
+  public String getVariantDuration(Request req, Response res) throws IOException, ParseException {
+    res.type(this.resDataType);
+    return this.analysis.getDuration();
+
+  }
+
+  public String getPossibleVariansAndSize(Request req, Response res) {
+    res.type(this.resDataType);
+    return this.analysis.getVariantsSize(medias, utils.getMediaIds(medias));
+  }
 }
